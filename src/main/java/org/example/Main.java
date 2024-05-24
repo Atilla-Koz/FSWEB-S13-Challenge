3adbd6c0d371925041d0dbf1d679907f5130c5a8
Atilla-Koz <atillakoz@hotmail.com>
2024-05-24 14:57:15 +0300

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import org.example.enums.Plan;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        runTasks();
+    }
+
+    public static void runTasks() {
+        Healthplan healthplan1 = new Healthplan(1, "A Sigorta", Plan.BASIC);
+        Healthplan healthplan2 = new Healthplan(2, "B Sigorta", Plan.PREMIUM);
+
+        String[] employeeHealthPlans = new String[2];
+        Employee employee = new Employee(1, "John Doe", "jd@test.com", "1234", employeeHealthPlans);
+
+        String[] companyEmployees = new String[2];
+        Company company = new Company(1, "Workintech", 1000, companyEmployees);
+
+        System.out.println(healthplan1);
+        System.out.println(healthplan2);
+
+        employee.addHealthPlan(0, healthplan1.getName());
+        employee.addHealthPlan(1, healthplan2.getName());
+        System.out.println(employee);
+
+        company.addEmployee(0, employee.getFullName());
+        System.out.println(company);
     }
 }
